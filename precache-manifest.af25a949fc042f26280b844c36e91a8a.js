4d128312021c2926c9b21b2eea91aae7cff95284
Katarzyna ZuberSmith <zuberka@hotmail.com>
2018-10-19 09:33:53 +0200

    Updates

---
+++ precache-manifest.af25a949fc042f26280b844c36e91a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0428903f76a68f26c0d",
+    "url": "/static/js/main.c0428903.chunk.js"
+  },
+  {
+    "revision": "261ff1d0ae466270604e",
+    "url": "/static/js/1.261ff1d0.chunk.js"
+  },
+  {
+    "revision": "c0428903f76a68f26c0d",
+    "url": "/static/css/main.6ed3742d.chunk.css"
+  },
+  {
+    "revision": "261ff1d0ae466270604e",
+    "url": "/static/css/1.cc84d059.chunk.css"
+  },
+  {
+    "revision": "80cfdcd12ce592a34a769151065740a9",
+    "url": "/index.html"
+  }
+];
